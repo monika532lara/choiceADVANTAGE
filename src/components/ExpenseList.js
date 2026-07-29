013a366e2bc19680afd4c9a93b8267b71a52a22e
monika532lara <monika532lara@outlook.com>
2026-07-29 09:55:30 +0530

    Create ExpenseList.js

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ExpenseList.css';
+
+function ExpenseList() {
+  return (
+    <div className="list">
+      <h2>Expense List</h2>
+      <p>No expenses yet.</p>
+    </div>
+  );
+}
+
+export default ExpenseList;
